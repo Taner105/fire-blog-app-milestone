c0356428677a01714e7472919ee2b0d846c37b16
Taner105 <taneraydogan59@gmail.com>
2022-01-14 17:59:35 +0300

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,51 @@
 import React from 'react'
 import cw from "../assests/cw.jpeg"
 import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 
 const Navbar = () => {
     const navigate = useNavigate();
+     const context = useContext(AuthContext)
     return (
         <div>
             <nav className='navbar navbar-expend-lg navbar-light bg-primary'>
-                <div>
-                   <a className=''><img style={{width:"50px",height:"50px",padding:"5px"}} src={cw} alt="Clarusway" /></a> 
+                <div className='container-fluid'>
+                   <a className='navbar-brand' onClick={() => navigate("/")}><img style={{cursor:"pointer",width:"50px",height:"50px",padding:"5px"}} src={cw} alt="Clarusway" /></a> 
+                   <p style={{color:"#fff", fontSize:"30px", cursor:"pointer",marginLeft:"80px"}} onClick={() => navigate("/")}>FİRE-BLOG</p>
                    <div className='buttons'>
-                        {
+                       {
+                           context.currentUser ? (
+                               <h3>{currentUser.displayName}</h3>
+                           ):(
                             <button
                             type='button'
                             className='ms-2 btn btn-outline-light'
                             onClick={() => navigate("/login")}
                             >
-
+                                Login
                             </button>
-                        }
+                           )
+                       }
+                       {
+                           context.currentUser ? (
+                               <button
+                               type='button'
+                               className='ms-2 btn btn-outline-light'
+                               >
+                                   Logout
+                               </button>
+                           ):(
+                               <button
+                               type='button'
+                               className='ms-2 btn btn-outline-light'
+                               onClick={() => navigate("/register")}
+                               >
+                                   Register
+                               </button>
+                               
+                           )
+                       }
                         
                    </div>
                 </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import blok from "../assests/blok.png"
 
 const Register = () => {
     return (
-        <div>
-            Register
+         <div className="register">
+            {/* <div className="form-image">
+                <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
+            </div> */}
+            
+            <div className="register-form">
+                <div className='blog-img'>
+                    <img src={blok} alt="blok" />
+                </div>
+                <h1 id='register' className="form-title display-8">— Register —</h1>
+                <form id="register">
+                <div className="mb-4">
+                    <label for="email" className="form-label display-4">Email</label>
+                    <input type="email" className="form-control" id="email" placeholder="Enter your email address..."
+                   
+                    />
+                </div>
+                <div className="mb-3">
+                    <label for="password" className="form-label display-4">Password</label>
+                    <input type="password" className="form-control" id="password" placeholder="Enter your password..." 
+                   
+                    />
+                </div>
+                <input type="button" className="btn btn-primary" value="Register" 
+                  
+                />
+                </form>
+            </div>
         </div>
     )
 }
